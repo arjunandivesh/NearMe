5ebf983358b4d2ddd08dbdf8d42c15d250e76a04
divesh <divesh27092006@gmail.com>
2025-12-05 13:49:03 +0530

    Success

--- mapproject/mapproject/wsgi.py
+++ mapproject/mapproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for divesh project.
+WSGI config for mapproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'divesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mapproject.settings')
 
 application = get_wsgi_application()
